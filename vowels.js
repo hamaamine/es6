3135581c10e661f2b9f5ad9be69a3c639ed36155
hamaamine <hamanacer32@gmail.com>
2024-11-04 08:55:34 +0000

    first commit

---
+++ vowels.js
@@ -0,0 +1,16 @@
+function countVowels() {
+    let person = prompt("Please enter your name");
+    const vowels = 'aeiouyAEIOUY'; 
+    let count = 0; 
+for (let char of person) {
+        if (vowels.includes(char)) { 
+            count++; 
+        }
+    }
+return count; 
+}
+
+const result = countVowels();
+alert(result); 
+
+
